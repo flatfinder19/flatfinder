0689d3d29aecef1b45b855024e0a9b61ac650a61
Natalie <natalieschennum@gmail.com>
2019-01-02 15:45:59 -0800

    Wrapping App,js Routes for Context

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AuthProvider, { AuthContext } from "./contexts/AuthProvider";
 
 import Map from './components/main/main';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
+      <div className="App container">
+        <AuthProvider>
+          <AuthContext.Consumer>
+            {context => (
           <Route path="/" component={Map} />
-        </Switch>
-      </Router>
+            )}
+          </AuthContext.Consumer>
+        </AuthProvider>
+      </div>
     );
   }
 }
