44882bdc8a60221deeb4fc20479eeacb7bf6af35
Maximo DeLaRosa <mjd809@gmail.com>
2019-01-02 18:28:25 -0500

    Fixes apiKey

--- web/src/components/main/map.js
+++ web/src/components/main/map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
+import keys from '../../keys.js';
 
 import Searchbar from './searchbar.js';
 
@@ -9,21 +10,23 @@ class MapContainer extends Component {
     return ( 
       <div className="Map">
         <Searchbar />
-        <Map google={this.props.google} zoom={14}>
-          <Marker onClick={this.onMarkerClick} name={'Current location'} />
+        <div>
+          <Map google={this.props.google} zoom={14}>
+            <Marker onClick={this.onMarkerClick} name={'Current location'} />
 
-          <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>'New York'</h1>
-            </div>
-          </InfoWindow>
-        </Map>
+            <InfoWindow onClose={this.onInfoWindowClose}>
+              <div>
+                <h1>'New York'</h1>
+              </div>
+            </InfoWindow>
+          </Map>
+        </div>
       </div>
      );
   }
 }
  
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCDd2rcf0hahjSqDslI2_f9ZsEU4ddKwXo'
+  apiKey: keys.googleKey
 })(MapContainer
 )
