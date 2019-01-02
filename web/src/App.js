d7ca247c03dffc1a0f4e65f01e1b0304f768289d
Maximo <mjd809@gmail.com>
2019-01-02 15:48:06 -0500

    Adds basic layout of map

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Map from './components/main/map';
+import Map from './components/main/main';
+
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-        <Route path="/" component={Map} />
+          <Route path="/" component={Map} />
         </Switch>
       </Router>
     );
