97c3bc74cc1337cc351bd3b70971171942a18e33
Maximo DeLaRosa <mjd809@gmail.com>
2019-01-02 18:15:47 -0500

    Adds googlemap API

--- web/src/components/main/map.js
+++ web/src/components/main/map.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
 
 import Searchbar from './searchbar.js';
 
-class Map extends Component {
+class MapContainer extends Component {
   state = {  }
   render() { 
     return ( 
       <div className="Map">
         <Searchbar />
-        <p> Map goes here </p>
+        <Map google={this.props.google} zoom={14}>
+          <Marker onClick={this.onMarkerClick} name={'Current location'} />
+
+          <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>'New York'</h1>
+            </div>
+          </InfoWindow>
+        </Map>
       </div>
      );
   }
 }
  
-export default Map;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCDd2rcf0hahjSqDslI2_f9ZsEU4ddKwXo'
+})(MapContainer
+)
