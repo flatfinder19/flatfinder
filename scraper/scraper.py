3cf4e347acaa230257a34bd95c4db4ff1929bae7
DanielMartinAlarcon <daniel@martinalarcon.org>
2019-01-03 10:14:58 -0800

    Settings now contains all default parameters

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,22 +1,50 @@
 from craigslist import CraigslistHousing
-from dateutil.parser import parse
-import time
 import settings
-
 
 def scrape():
     """
-    Scrapes craigslist for a certain geographic area, and finds the latest listings.
-    :param area:
-    :return: A list of results.
+    Scrapes craigslist using the parameters in settings.py
+    :return: A list of results, each as a dictionary.
+                Length of dictionary given by MAX_LISTINGS, below
     """
-    cl_h = CraigslistHousing(site=settings.CRAIGSLIST_SITE, category=settings.CRAIGSLIST_HOUSING_SECTION,
-                             filters={'max_price': settings.MAX_PRICE, "min_price": settings.MIN_PRICE})
+    # How many of these to scrape at once.
+    MAX_LISTINGS = 5
+
+    filters = {'query':settings.query,
+                 'search_titles':settings.search_titles,
+                 'has_image':settings.has_image,
+                 'posted_today':settings.posted_today,
+                 'bundle_duplicates':settings.bundle_duplicates,
+                 'private_room':settings.private_room,
+                 'private_bath':settings.private_bath,
+                 'cats_ok':settings.cats_ok,
+                 'dogs_ok':settings.dogs_ok,
+                 'min_price':settings.min_price,
+                 'max_price':settings.max_price,
+                 'min_ft2':settings.min_ft2,
+                 'max_ft2':settings.max_ft2,
+                 'min_bedrooms':settings.min_bedrooms,
+                 'max_bedrooms':settings.max_bedrooms,
+                 'min_bathrooms':settings.min_bathrooms,
+                 'max_bathrooms':settings.max_bathrooms,
+                 'no_smoking':settings.no_smoking,
+                 'is_furnished':settings.is_furnished,
+                 'wheelchair_acccess':settings.wheelchair_acccess,
+                 'housing_type':settings.housing_type,
+                 'laundry':settings.laundry,
+                 'parking':settings.parking,
+                 }
+    
+    
+    cl_h = CraigslistHousing(site=settings.cl_site, 
+                             category=settings.housing_category, # Apartments for rent only
+                             area=settings.sub_city_area,
+                             filters=filters)
 
     results = []
     gen = cl_h.get_results(sort_by='newest', 
                            geotagged=True, 
-                           limit=settings.MAX_LISTINGS,
+                           limit=MAX_LISTINGS,
                            include_details=True)
     
     while True:
@@ -39,6 +67,3 @@ def scrape():
         results.append(result)
 
     return results
-
-listings = scrape()
-listings
--- scraper/settings.py
+++ scraper/settings.py
@@ -1,37 +1,65 @@
-## Price
 
-# The minimum rent you want to pay per month.
-MIN_PRICE = 1500
-
-# The maximum rent you want to pay per month.
-MAX_PRICE = 2000
+### These are the parameters set by the user, each with default values.
 
 ## Location preferences
 
-# The Craigslist site you want to search on.
-# For instance, https://sfbay.craigslist.org is SF and the Bay Area.
-# You only need the beginning of the URL.
-CRAIGSLIST_SITE = 'sfbay'
+# Choose from the list of cities
+cl_site = 'sfbay'
 
-## Transit preferences
+# Choose from the list of areas, available for some cities
+sub_city_area = 'sfc'
 
-# The farthest you want to live from a transit stop.
-MAX_TRANSIT_DIST = 2 # kilometers
+# Choose one only, from a dropdown menu.
+housing_category = 'apa'
+# {
+# 'apa':'apartments/housing for rent',
+# 'sub':'sublets and temporary',
+# 'prk':'parking and storage',
+# 'roo':'rooms and shares',
+# 'off':'office and commercial',
+# 'sbw':'sublet/temp wanted',
+# 'hou':'apartments wanted',
+# 'swp':'housing swap',
+# 'reb':'real estate by broker',
+# 'reo':'real estate by owner',
+# 'rew':'real estate wanted',
+# 'sha':'rooms wanted',
+# 'vac':'vacation rentals',
+# 'hhh':'all'
+# }
 
-## Search type preferences
 
-# The Craigslist section underneath housing that you want to search in.
-# For instance, https://sfbay.craigslist.org/search/apa find apartments for rent.
-# https://sfbay.craigslist.org/search/sub finds sublets.
-# You only need the last 3 letters of the URLs.
-CRAIGSLIST_HOUSING_SECTION = 'apa'
+## Apartment filters
+query = None # string
+search_titles = False # Query searches titles only.
+has_image = False 
+posted_today = False
+bundle_duplicates = False
+private_room = False
+private_bath = False
+cats_ok = False
+dogs_ok = False
+min_price = 0
+max_price = 0
+min_ft2 = 0
+max_ft2 = 0
+min_bedrooms = 0
+max_bedrooms = 0
+min_bathrooms = 0
+max_bathrooms = 0
+no_smoking = False
+is_furnished = False
+wheelchair_acccess = False
+# Checklist should produce a list of strings. 
+# Options: ['apartment', 'condo', 'cottage/cabin', 'duplex', 'flat', 'house', 'in-law', 'loft', 'townhouse', 'manufactured', 'assisted living', 'land']
+housing_type = []
+# Checklist should produce a list of strings. 
+# Options:  ['w/d in unit', 'w/d hookups', 'laundry in bldg', 'laundry on site', 'no laundry on site']
+laundry = []
+# Checklist should produce a list of strings.
+# Options:  ['carport', 'attached garage', 'detached garage', 'off-street parking', 'street parking', 'valet parking', 'no parking']
+parking = []
 
-## System settings
 
-# How long we should sleep between scrapes of Craigslist.
-# Too fast may get rate limited.
-# Too slow may miss listings.
-SLEEP_INTERVAL = 20 * 60 # 20 minutes
 
-# How many results to retrieve
-MAX_LISTINGS = 5
+
