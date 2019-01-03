e73edae627ca233155831424ccd158234970a337
Natalie <natalieschennum@gmail.com>
2019-01-02 16:57:01 -0800

    Noted Out Routes on App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AuthProvider, { AuthContext } from "./contexts/AuthProvider";
 
-import Map from './components/main/main';
+import Map from "./components/main/main";
 
 class App extends Component {
   render() {
@@ -11,7 +11,19 @@ class App extends Component {
         <AuthProvider>
           <AuthContext.Consumer>
             {context => (
-          <Route path="/" component={Map} />
+              <React.Fragment>
+                {/* <Route
+                  exact
+                  path="/"
+                  render={props => <LoginPage {...props} context={context} />}
+                />
+                <Route
+                  exact
+                  path="/register"
+                  render={props => <Register {...props} context={context} />}
+                /> */}
+                <Route path="/" component={Map} />
+              </React.Fragment>
             )}
           </AuthContext.Consumer>
         </AuthProvider>
