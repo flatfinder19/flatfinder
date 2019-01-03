0bcd4371ffa8bb180d99f81e53324e9decfb790e
Natalie <natalieschennum@gmail.com>
2019-01-02 16:57:21 -0800

    Refactored Index.js

--- web/src/index.js
+++ web/src/index.js
@@ -1,22 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import App from './App';
-import rootReducer from './reducers';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 require('./sass/main.css');
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk)),
-);
 
 ReactDOM.render(
-  <Provider store={store}>
+  <BrowserRouter>
     <App />
-  </Provider>,
-  document.getElementById('root'),
+  </BrowserRouter>,
+  document.getElementById("root")
 );
