217a595458a93e860fdc931d278a27adaf16a9e5
Maximo DeLaRosa <mjd809@gmail.com>
2019-01-02 22:47:17 -0500

    Adds sidebar options

--- web/src/components/main/sidebar.js
+++ web/src/components/main/sidebar.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
+const style= {
+  marginRight: '1rem', 
+  background: 'EA4336'
+}
+
 class Map extends Component {
   state = {  }
   render() { 
     return ( 
       <div className="Sidebar">
-        <p>Sidebar </p>
+        <div className="Sidebar-title">
+          <p>Flatfinder</p>
+        </div>
+        <div className='options'>
+          <p style={{color: '#616161', fontSize:'13px'}}>Options</p>
+          <p><i style={style} className="fas fa-clock icon-color"></i>Posted today</p>
+          <p><i style={style} className= "fas fa-dog icon-color"></i>Dogs</p>
+          <p></p>
+          <p style={{color: '#616161', fontSize:'13px'}}>Advanced Options</p>
+        </div>
       </div>
      );
   }
