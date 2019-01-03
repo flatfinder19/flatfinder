a4732067f45dd5b2a18bd7887a5ec015389bb2c0
Edward Manda <lilkups@yahoo.com>
2019-01-03 10:09:16 -0600

    fixed

--- web/src/components/auth/login.js
+++ web/src/components/auth/login.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { NavLink, Link, withRouter } from 'react-router-dom';
 
 // import logo from './google.png';
