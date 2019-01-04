03ec3fb8d71e262ca088f29ac1ff487ae7289c74
Natalie <natalieschennum@gmail.com>
2019-01-03 16:09:00 -0800

    Work on side-bar tab toggle

--- web/src/components/main/sidebar.js
+++ web/src/components/main/sidebar.js
@@ -11,6 +11,11 @@ class Map extends Component {
   state = { 
     posted: false,
     dogs: false,
+    cats: false, 
+    furnished: false, 
+    nonSmoking: false, 
+    accessible: false, 
+    parking: false, 
   };
 
   active = (option) => {
@@ -21,6 +26,21 @@ class Map extends Component {
       case 'dogs':
         this.setState({dogs: !this.state.dogs })
         break;
+      case 'cats':
+        this.setState({cats: !this.state.cats })
+        break;
+      case 'furnished':
+        this.setState({furnished: !this.state.furnished })
+        break;
+      case 'non-smoking':
+        this.setState({nonSmoking: !this.state.nonSmoking })
+        break;
+      case 'accessible':
+        this.setState({accessible: !this.state.accessible })
+        break;
+      case 'parking':
+        this.setState({parking: !this.state.parking })
+        break;
       default:
         // code block
     }
@@ -30,7 +50,7 @@ class Map extends Component {
     const style = {
       marginRight: '1rem',
       cursor: 'pointer',
-      color: '#EA4336'
+      color: '#EA4336',
     }
     const Activestyle = {
       marginRight: '1rem',
@@ -54,10 +74,33 @@ class Map extends Component {
               : style} className= "fas fa-dog"></i>
             Dogs
           </p>
-          <p></p>
+          <p onClick={() => this.active('cats')}>
+            <i style={this.state.cats ? Activestyle 
+              : style}
+              className="fas fa-cat"></i>
+            Cats</p>
+          <p onClick={() => this.active('furnished')}>
+            <i style={this.state.furnished ? Activestyle 
+              : style} className= "fas fa-couch"></i>
+            Furnished
+          </p>
+          <p onClick={() => this.active('non-smoking')}>
+            <i style={this.state.nonSmoking ? Activestyle 
+              : style} className= "fas fa-smoking-ban"></i>
+            Non-Smoking
+          </p>
+          <p onClick={() => this.active('accessible')}>
+            <i style={this.state.accessible ? Activestyle 
+              : style} className= "fas fa-wheelchair"></i>
+              Accessible
+            </p>
+            <p onClick={() => this.active('parking')}>
+            <i style={this.state.parking ? Activestyle 
+              : style} className= "fas fa-car"></i>
+              Parking
+            </p>
           <p style={{color: '#616161', fontSize:'13px'}}>Advanced Options</p>
         </div>
-       <FlatFeatures/>
       </div>
      );
   }
