2bfe1646f26b4f3230392af641848b83df429bb6
Edward Manda <lilkups@yahoo.com>
2019-01-03 22:27:17 -0600

    added some cities

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AuthProvider, { AuthContext } from './contexts/AuthProvider';
-
+import axios from 'axios';
 import Map from './components/main/main';
 import Login from './components/auth/login';
 
