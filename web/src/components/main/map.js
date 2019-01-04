ea693f4b094d681cd277fc3c3024a4d1d8041220
Maximo DeLaRosa <mjd809@gmail.com>
2019-01-04 13:59:44 -0500

    Adds fetchdata experiement

--- web/src/components/main/map.js
+++ web/src/components/main/map.js
@@ -3,6 +3,7 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import keys from '../../keys.js';
 import Searchbar from './searchbar.js';
 import Modal from './modal.js';
+import axios from 'axios';
 
 //const keys = { googleKey: process.env.REACT_APP_GOOGLE_KEY };
 console.log(keys);
@@ -14,6 +15,40 @@ class MapContainer extends Component {
   state = { 
     showModal: false,
    }
+
+  // async componentDidMount() {
+  //   const data = {
+  //     query: '',
+  //     "latitude": "40.714199",
+  //     "longitude": "-74.006401",
+  //     "laundry": true,
+  //     "parking": false,
+  //     "price": "1,500",
+  //     "bedrooms": 2,
+  //     "has_image": false,
+  //     "cats_ok": false,
+  //     "dogs_ok": true,
+  //     "square_ft": "",
+  //     "no_of_bathrooms": 1,
+  //     "no_smoking": false,
+  //     "is_furnished": false,
+  //     "wheelchair_acccess": false,
+  //     "housing_type": ""
+  //   };
+  //   const token = localStorage.getItem('token');
+  //   console.log(token);
+  //   const res = await axios
+  //     .post("https://flatfinderapp.herokuapp.com/api/personalsearches/",
+  //     data,
+  //     {
+  //       headers: {
+  //         Authorization: `JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNTQ2NjI3NDE2LCJlbWFpbCI6IiJ9.--xKYIQBHbX112u8aC7IDbGUQbqg7XzEwA04mVwxBkQ`
+  //       }
+  //     }
+  //   );
+  //   const response = res.data;
+  //   console.log(response);
+  // }
 
   showModal = () => {
     this.setState({ 
