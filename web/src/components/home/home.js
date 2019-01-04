1be766642fbfd26807490f65c069bda3ad481f23
Maximo DeLaRosa <mjd809@gmail.com>
2019-01-03 19:06:58 -0500

    Adds home topNav

--- web/src/components/home/home.js
+++ web/src/components/home/home.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   state = {  }
   render() { 
     return ( 
-      <div>
-
+      <div className="Home">
+        <div className='Navigation'>
+          <div className="Navigation-box">
+            <p className="Navlogo">Flatfinder</p>
+            <div>
+              <Link to='/login'><p>Login</p></Link>
+              <Link to='/signup'><p>Register</p></Link>
+            </div>
+          </div>
+        </div>
       </div>
      );
   }
