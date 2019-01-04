7dd5aaf6ebf00a2c6847bdc9739760d52d3689af
Maximo DeLaRosa <mjd809@gmail.com>
2019-01-04 01:45:09 -0500

    Adds lorem-ipsum in about

--- web/src/components/home/home.js
+++ web/src/components/home/home.js
@@ -23,8 +23,17 @@ class Home extends Component {
 
         </div>
         <div className="Home-description">
+          <p className="Home-description-title">About</p>
+          <p className="Home-description-details">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+            labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+             nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+            dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+            deserunt mollit anim id est laborum.
+          </p>
         </div>
         <div className="Home-testimonials">
+          <p className="Home-description-title">Testimonials</p>
         </div>
       </div>
      );
